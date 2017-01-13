ad3f3af7a40ddff02f33ddffea52d53f7921dad2
Gregory Smith, II <Greg.W.Smith@nordstrom.com>
2017-01-12 17:24:51 -0800

    Simple test spec and ESLint configs for test.

---
+++ products/test/product-sources.spec.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const expect = require('chai').expect;
+const ProductSource = require('../scrape-store-website-product-source').ProductSource;
+
+describe('The store scraping from the website ProductSource', function () {
+  it('class is exported', function () {
+    expect(ProductSource).to.not.be.null;
+  });
+});
