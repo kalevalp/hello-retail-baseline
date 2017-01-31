8d3e586301fb4c57e828d399b314a2eca987cf8a
Erik Erikson <erik.m.erikson@nordstrom.com>
2017-01-31 14:05:15 -0800

    Merge branch 'initial-formation' into initial-formation-bikeshedding
    # eslint formatting conflicts resolutions

--- products/lambda/scrape-products/kinesis-event-writer.js
+++ products/lambda/scrape-products/kinesis-event-writer.js
@@ -1,33 +1,33 @@
-'use strict';
+'use strict'
 
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
+const AWS = require('aws-sdk') // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
 
 class KinesisEventWriter {
   constructor() {
-    this.kinesis = new AWS.Kinesis();
-    this.kinesis.config.region = AWS.config.region;
-    this.kinesis.config.credentials = AWS.config.credentials;
+    this.kinesis = new AWS.Kinesis()
+    this.kinesis.config.region = AWS.config.region
+    this.kinesis.config.credentials = AWS.config.credentials
   }
 
   writeKinesisEvent(data, partitionKey) {
-    const envelopeEvent = KinesisEventWriter.envelopeEvent();
-    envelopeEvent.data = data;
+    const envelopeEvent = KinesisEventWriter.envelopeEvent()
+    envelopeEvent.data = data
 
     const newProductCreatedEvent = {
       Data: JSON.stringify(envelopeEvent),
       PartitionKey: partitionKey,
       StreamName: process.env.STREAM_NAME,
-    };
+    }
 
     this.kinesis.putRecord(newProductCreatedEvent, (err, ack) => {
       if (ack) {
-        console.log(`K-PUT: ${JSON.stringify(ack)}`);
+        console.log(`K-PUT: ${JSON.stringify(ack)}`)
       }
 
       if (err) {
-        throw new Error(err);
+        throw new Error(err)
       }
-    });
+    })
   }
 }
 
@@ -35,6 +35,6 @@ KinesisEventWriter.envelopeEvent = () => ({
   schema: 'com.nordstrom/retail-stream-ingress/1-0-0',
   origin: 'hello-retail/product-scraper',
   timeOrigin: new Date().toISOString(),
-});
+})
 
-module.exports = KinesisEventWriter;
+module.exports = KinesisEventWriter
--- products/lambda/scrape-products/scrape-products.js
+++ products/lambda/scrape-products/scrape-products.js
@@ -1,25 +1,25 @@
-'use strict';
+'use strict'
 
-const AWS = require('aws-sdk'); // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
-const ProductSource = require('./scrape-store-website-product-source');
-const Product = require('../product');
-const ProductEvents = require('../product-events');
+const AWS = require('aws-sdk') // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
+const ProductSource = require('./scrape-store-website-product-source')
+const Product = require('../product')
+const ProductEvents = require('../product-events')
 
 exports.handler = (event, context, callback) => {
-  const ps = new ProductSource();
-  const productCount = 0;
-  const errorCount = 0;
+  const ps = new ProductSource()
+  const productCount = 0
+  const errorCount = 0
 
-  const kinesis = new AWS.Kinesis();
+  const kinesis = new AWS.Kinesis()
 
   kinesis.config.credentials = new AWS.TemporaryCredentials({
     RoleArn: process.env.STREAM_WRITER_ROLE,
-  });
+  })
 
   if (!event
       || !event.lambdaTimeout || !(typeof event.lambdaTimeout === 'number') || event.lambdaTimeout <= 0
       || !event.productFrequency || !(typeof event.productFrequency === 'number') || event.productFrequency <= 0) {
-    throw new Error('Invalid timeout or scrap frequency. Both must be positive numbers.');
+    throw new Error('Invalid timeout or scrap frequency. Both must be positive numbers.')
   }
 
   const oneProductInterval = setInterval(() => {
@@ -29,14 +29,14 @@ exports.handler = (event, context, callback) => {
         scraped.Title,
         scraped.Brand.Label,
         scraped.category,
-        `PAGE:${scraped.ProductPageUrl}`);
+        `PAGE:${scraped.ProductPageUrl}`)
 
-      ProductEvents.sendCreateEvent(product);
-    });
-  }, event.productFrequency);
+      ProductEvents.sendCreateEvent(product)
+    })
+  }, event.productFrequency)
 
   setTimeout(() => {
-    clearInterval(oneProductInterval);
-    callback(null, `${productCount} products produced, ${errorCount} errors`);
-  }, event.lambdaTimeout);
-};
+    clearInterval(oneProductInterval)
+    callback(null, `${productCount} products produced, ${errorCount} errors`)
+  }, event.lambdaTimeout)
+}
