1a056260ed038ff1ea25a4d9ad7ed92bd1c0de86
Erik Erikson <erik.m.erikson@nordstrom.com>
2017-02-02 12:49:19 -0800

    Better debug reporting during build

--- build/1.install.sh
+++ build/1.install.sh
@@ -3,8 +3,14 @@
 echo $1 in $2
 if cd $2
 then
-  npm install
-  exit $?
+  if npm install
+  then
+    exit $?
+  else
+    NPM_INSTALL_RESULT=$?
+    tail -n 100 npm-debug.log
+    exit $NPM_INSTALL_RESULT
+  fi
 else
   exit $?
 fi
