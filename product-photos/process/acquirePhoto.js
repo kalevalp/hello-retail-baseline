22151c77df221242d73df8405396f764a2b9ae92
Erik Erikson <erik.m.erikson@nordstrom.com>
2017-02-25 01:55:02 -0800

    finish file name fix

--- product-photos/process/acquirePhoto.js
+++ product-photos/process/acquirePhoto.js
@@ -5,7 +5,7 @@ const yaml = require('js-yaml') // eslint-disable-line import/no-extraneous-depe
 
 module.exports = {
   shim: () => {
-    const aquirePhoto = yaml.safeLoad(fs.readFileSync('./aquirePhoto.yml', 'utf8'))
+    const aquirePhoto = yaml.safeLoad(fs.readFileSync('./acquirePhoto.yml', 'utf8'))
     return JSON.stringify(aquirePhoto)
   },
 }
