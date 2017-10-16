bd57f307985b67606182427b993df4cb492e9ba0
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-15 18:00:34 -0700

    more cleanup of install and deployment scripts

--- build/2.sls.sh
+++ build/2.sls.sh
@@ -12,7 +12,7 @@ OWD=$(pwd)
 cd ${3}
 RES=$?
 if [ ${RES} -eq 0 ]; then
-  ${OWD}/node_modules/.bin/sls ${1} -s ${STAGE} -v
+  /usr/local/bin/sls ${1} -s ${STAGE} -v
   RES=$?
   cd ${OWD}
   exit ${RES}
