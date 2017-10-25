d974c1a2845178d3ac9011f959932f705bdaaa6f
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-24 22:19:44 -0700

    API lambda that initiates the product purchase step function.

--- product-purchase/0.purchaseApi/purchaseApi.js
+++ product-purchase/0.purchaseApi/purchaseApi.js
@@ -1,65 +1,30 @@
 'use strict';
 
-const KH = require('kinesis-handler');
-
-const eventSchema = require('./retail-stream-schema-ingress.json');
-const productPurchaseSchema = require('./product-purchase-schema.json');
-
-const { KV_Store } = require('kv-store');
-const fs = require('fs');
-
-const conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
-
+const aws = require('aws-sdk'); // eslint-disable-line import/no-unresolved, import/no-extraneous-dependencies
 
 const constants = {
-  MODULE: 'product-purchase/purchaseApi.js',
-  TABLE_CREDIT_CARDS_NAME: 'creditCardsTable',
-
+  STEP_FUNCTION: process.env.STEP_FUNCTION,
 };
-const impl = {
-  processPurchase: (event, callback) => {
-    const kv = new KV_Store(conf.host, conf.user, conf.pass, constants.TABLE_CREDIT_CARDS_NAME);
 
-    if (event.creditCard) {
-      //
-      // Charge credit card.
-      // Contact warehouse.
+const stepfunctions = new aws.StepFunctions();
 
-      if (event.storeCreditCard) {
-        if (event.user && event.password) {
-          kv.init()
-            .then(() => kv.put(event.user + event.password, event.creditCard))
-            .then(() => kv.close())
-            .then(callback())
-            .catch(err => callback(err));
-        } else {
-          callback('Can\'t store credit card information without valid identification.');
-        }
+module.exports.handler = (event, context, callback) => {
+  console.log(event);
+  const params = {
+    stateMachineArn: constants.STEP_FUNCTION,
+    name: `${event.data.id}/${event.data.user}`,
+    input: JSON.stringify(event.data),
+  };
+  stepfunctions.startExecution(params, (err) => {
+    if (err) {
+      if (err.code && err.code === 'ExecutionAlreadyExists') {
+        callback()
       } else {
-        callback();
+        callback(err)
       }
-    } else if (event.user && event.password) {
-      kv.init()
-        .then(() => kv.get(event.user + event.password))
-        .then(res => kv.close().then(() => res))
-        .then(res =>
-          //
-          // Charge credit card.
-          // Contact warehouse.
-           res)
-        .then(() => callback())
-        .catch(err => callback(err))
     } else {
-      callback('No credit card or identity details received.')
+      callback()
     }
-  },
-
-};
-
-const kh = new KH.KinesisHandler(eventSchema, constants.MODULE);
-kh.registerSchemaMethodPair(productPurchaseSchema, impl.processPurchase);
-
-module.exports = {
-  processPurchase: kh.processKinesisEvent.bind(kh),
+  })
 };
 
