b78b37ef9b10536033618e8a8e808fc40a2ef1ad
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-24 21:44:08 -0700

    Price query lambda.

---
+++ product-purchase/2.get-price/getPrice.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const { KV_Store } = require('kv-store');
+const fs = require('fs');
+
+const conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
+
+const constants = {
+  TABLE_PRODUCT_CATALOG_NAME: 'productCatalogTable',
+};
+
+
+module.exports.authenticate = (event, context, callback) => {
+  const kv = new KV_Store(conf.host, conf.user, conf.pass, constants.TABLE_PRODUCT_CATALOG_NAME);
+  const result = event;
+
+  kv.init()
+    .then(kv.get(event.productID))
+    .then(res => kv.close().then(() => res))
+    .then((res) => {
+      if (res) {
+        const price = JSON.parse(res).price;
+
+        if (price) {
+          result.gotPrice = 'true';
+          result.price = price;
+        } else {
+          result.gotPrice = 'false';
+          result.failureReason = 'No price in the catalog';
+        }
+      } else {
+        result.gotPrice = 'false';
+        result.failureReason = 'Product not in catalog';
+      }
+      callback(null, result)
+    })
+    .catch(err => callback(err))
+};
