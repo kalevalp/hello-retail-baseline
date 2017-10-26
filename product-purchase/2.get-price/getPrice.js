d765d16c66a274d2996d55a1e028fc948f8570de
Kalev Alpernas <kalev.alp@gmail.com>
2017-10-26 11:04:36 -0700

    Fixed getPrice in the purchase subsystem.

--- product-purchase/2.get-price/getPrice.js
+++ product-purchase/2.get-price/getPrice.js
@@ -6,12 +6,12 @@ const fs = require('fs');
 const conf = JSON.parse(fs.readFileSync('conf.json', 'utf8'));
 
 const constants = {
-  TABLE_PRODUCT_CATALOG_NAME: process.env.TABLE_PRODUCT_CATALOG_NAME,
+  TABLE_PRODUCT_PRICE_NAME: process.env.TABLE_PRODUCT_PRICE_NAME,
 };
 
 
 module.exports.handler = (event, context, callback) => {
-  const kv = new KV_Store(conf.host, conf.user, conf.pass, constants.TABLE_PRODUCT_CATALOG_NAME);
+  const kv = new KV_Store(conf.host, conf.user, conf.pass, constants.TABLE_PRODUCT_PRICE_NAME);
   const result = event;
 
   kv.init()
@@ -19,11 +19,11 @@ module.exports.handler = (event, context, callback) => {
     .then(res => kv.close().then(() => res))
     .then((res) => {
       if (res) {
-        const price = JSON.parse(res).price;
+        // const price = JSON.parse(res).price;
 
-        if (price) {
+        if (res) {
           result.gotPrice = 'true';
-          result.price = price;
+          result.price = res;
         } else {
           result.gotPrice = 'false';
           result.failureReason = 'No price in the catalog';
